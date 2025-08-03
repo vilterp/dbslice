561b584df80483dcdc0463ff242eb0d1b1edf0d6
Pete Vilter <pete.vilter@gmail.com>
2025-08-02 23:50:35 -0400

    cursor pointer

--- client/src/TabBar.tsx
+++ client/src/TabBar.tsx
@@ -46,6 +46,7 @@ const TabBar: React.FC<TabBarProps> = ({ tabs, selectedTabId, onTabClick, onTabC
         background: "#f8f9fa",
         borderBottom: "1px solid #e0e0e0",
         alignItems: "center",
+        cursor: "pointer",
       }}
     >
       {tabs.map((tab) => (
